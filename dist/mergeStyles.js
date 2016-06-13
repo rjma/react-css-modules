61124ffd83666f39552e130a4794df95d189df27
Ruijin Ma <rjma@live.com>
2016-06-13 15:47:20 +0800

    add dist

---
+++ dist/mergeStyles.js
@@ -0,0 +1,29 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _assign2 = require('lodash/assign');
+
+var _assign3 = _interopRequireDefault(_assign2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function (defaultStyles, styles) {
+    var mergedStyles = (0, _assign3.default)({}, defaultStyles);
+
+    for (var p in mergedStyles) {
+        if (!mergedStyles.hasOwnProperty(p)) {
+            continue;
+        }
+        if (styles[p]) {
+            mergedStyles[p] += ' ' + styles[p];
+        }
+    }
+
+    return mergedStyles;
+};
+
+module.exports = exports['default'];
+//# sourceMappingURL=mergeStyles.js.map
